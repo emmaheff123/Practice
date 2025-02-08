ad18f7905a7d1aa2c8e7dd58e32da5e336d097aa
emmaheff123 <emheffernan1@gmail.com>
2025-02-08 10:51:46 -0500

    two_sum better runtime

--- two_sum.py
+++ two_sum.py
@@ -7,3 +7,12 @@ def twoSum(nums, target):
                 indices.append(i)
                 indices.append(j)
                 return indices
+            
+
+def twoSum(nums, target):
+    num_map = {} 
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]  
+        num_map[num] = i
