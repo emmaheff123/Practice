6ff22b6a9d78cd478ca52758f04a48b4d365ac71
emmaheff123 <emheffernan1@gmail.com>
2025-02-08 11:03:15 -0500

    two_sum java better runtime

--- two_sum.java
+++ two_sum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public int[] twoSum(int[] nums, int target) {
     int n = nums.length;
     for (int i = 0; i < n; i++){
@@ -8,4 +10,17 @@ public int[] twoSum(int[] nums, int target) {
         }
     }
     return new int[]{};
+}
+
+
+public int[] twoSum(int[] nums, int target) {
+    HashMap<Integer, Integer> numMap = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (numMap.containsKey(complement)) {
+            return new int[]{numMap.get(complement), i}; // Found the pair
+        }
+        numMap.put(nums[i], i); // Store index of the current number
+    }
+    return new int[]{}; // No solution found
 }
